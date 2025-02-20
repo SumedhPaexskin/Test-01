5d7cce34f133e8c6dd27f92728bc8f11b3059b5e
sumedh hable <sumedh.hable@paexskin.com>
2025-02-20 22:48:32 +0530

    this is login php

--- backend/login.php
+++ backend/login.php
@@ -1,8 +1,10 @@
 <?php
+header("Access-Control-Allow-Origin: http://127.0.0.1:5500");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Headers: Content-Type");
+header('Content-Type: application/json');
+ 
 
 
 $servername = "127.0.0.1"; 
